5f0b042eea05431d0678c973c48ffca20081d44f
徐明浩 <871922383@qq.com>
2019-04-28 22:09:02 +0800

    第一版本，实现简单的左右移动

---
+++ src/com/TA/TF.java
@@ -0,0 +1,109 @@
+package com.TA;
+
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TF extends Frame {
+	
+	int x = 200, y = 200;
+	
+	public TF() {
+		setSize(800, 600);
+		setResizable(false);
+		setTitle("tank war");
+		setVisible(true);
+		
+		this.addKeyListener(new MyKeyListener());
+		
+		addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+			
+		});
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		g.fillRect(x, y, 50, 50);
+		//x += 10;
+		//y += 10;
+	}
+	
+	
+	class MyKeyListener extends KeyAdapter {
+		
+		boolean bL = false;
+		boolean bU = false;
+		boolean bR = false;
+		boolean bD = false;
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			int key = e.getKeyCode();
+			switch (key) {
+			case KeyEvent.VK_LEFT:
+				bL = true;
+				break;
+			case KeyEvent.VK_UP:
+				bU = true;
+				break;
+			case KeyEvent.VK_RIGHT:
+				bR = true;
+				break;
+			case KeyEvent.VK_DOWN:
+				bD = true;
+				break;
+
+			default:
+				break;
+			}
+			if (bU) {
+				y-=10;
+			}
+			if (bD) {
+				y+=10;
+			}
+			if (bL) {
+				x-=10;
+			}
+			if (bR) {
+				x+=10;
+			}
+			
+			repaint();
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			int key = e.getKeyCode();
+			switch (key) {
+			case KeyEvent.VK_LEFT:
+				bL = false;
+				break;
+			case KeyEvent.VK_UP:
+				bU = false;
+				break;
+			case KeyEvent.VK_RIGHT:
+				bR = false;
+				break;
+			case KeyEvent.VK_DOWN:
+				bD = false;
+				break;
+
+			default:
+				break;
+			}
+		}
+		
+	}
+	
+	
+}
---
+++ src/com/TA/TT.java
@@ -0,0 +1,15 @@
+package com.TA;
+
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TT {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		TF tf=new TF();
+		
+		
+	}
+}
